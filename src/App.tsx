a58c1530ed471c3f960a593984b59ec6b2c374ab
Andrew <167503082+OpiaUK@users.noreply.github.com>
2024-04-24 17:13:19 +0000

    finished skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>OPIA</h1>
+      <h2>a human centric operating system that powers the world around you and beyond.</h2>
+      <p>
+        view the whitepaper: https://docs.google.com/document/d/1IcBpwH_XpYzAxiEEiHttPfQPYIK7EBDOXbli6zIrj8A/edit?usp=sharing
+      </p>
+      <p>
+        buy tokens via telegram @thisisOPIA
+
+      </p>
+      <p>
+        vote on our community roadmap on Telegram
+      </p>
     </div>
   );
 }
